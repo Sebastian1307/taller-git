506c56d58d7690cdad12f73722995028a9d07894
Sebastian Beltran <sebastianbeltran882@gmail.com>
2025-08-08 10:56:37 -0500

    First Commit: game.js Uploaded

---
+++ game.js
@@ -0,0 +1,23 @@
+const NumeroXD = Math.floor(Math.random() * 100) + 1;
+let intentos = 0;
+
+function RevisarRespuesta() {
+    const input = document.getElementById('InputAdivinar');
+    const mensaje = document.getElementById('Kirbodice');
+    const respuesta = Number(input.value);
+    intentos++;
+
+    const diferencia = Math.abs(NumeroXD - respuesta);
+
+    if (respuesta === NumeroXD) {
+        mensaje.textContent = `¡Sisas! Lo adivino en ${intentos} intentos. Casi que no manco`;
+    } else if (diferencia <= 5) {
+        mensaje.textContent = "¡Mas caliente que adolescente de colegio publico! ";
+    } else if (diferencia <= 10) {
+        mensaje.textContent = "Caliente como el guaviare ";
+    } else if (diferencia <= 20) {
+        mensaje.textContent = "Frio como un milo ";
+    } else {
+        mensaje.textContent = "¡Tan frio que parece de millos! ";
+    }
+}
